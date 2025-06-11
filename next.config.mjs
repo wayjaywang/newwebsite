77ec9cb33f5eb3cfc329a72240289d9c70af7e4d
wayjaywang <wayjaywang@gmail.com>
2025-06-11 17:44:31 -0400

    fix: Add assetPrefix for GH pages styles

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   basePath: isProd ? '/newwebsite' : '',
+  assetPrefix: isProd ? '/newwebsite/' : '',
   images: {
     unoptimized: true,
   },
